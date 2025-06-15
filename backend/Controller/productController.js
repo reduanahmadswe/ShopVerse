ed766c7a66df02a488e1c40f0c0c6b54db3826d5
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-15 18:10:32 +0600

    feat: add error handling middleware and refactor product controller for improved error management

--- backend/Controller/productController.js
+++ backend/Controller/productController.js
@@ -1,4 +1,5 @@
 import Product from "../models/productModel.js";
+import HandleError from "../utils/handleError.js";
 
 //Creating Product
 export const createProducts = async (req, res) => {
@@ -39,16 +40,13 @@ export const getAllProducts = async (req, res) => {
 };
 
 //Update product
-export const updateProduct = async (req, res) => {
+export const updateProduct = async (req, res,next) => {
     const products = await Product.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
     });
     if (!products) {
-        return res.status(404).json({
-            success: false,
-            message: "Product not found",
-        });
+        return next(new HandleError("Product not found", 404));
     }
     res.status(200).json({
         success: true,
@@ -61,11 +59,8 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
 
     const product = await Product.findByIdAndDelete(req.params.id);
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: "Product not found",
-        });
+        if (!product) {
+        return next(new HandleError("Product not found", 404));
     }
 
     res.status(200).json({
@@ -78,11 +73,9 @@ export const deleteProduct = async (req, res) => {
 //Accessing a single product
 export const getSingleProduct = async (req, res) => {
     const product = await Product.findById(req.params.id);
+
     if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: "Product not found",
-        });
+        return next(new HandleError("Product not found", 404));
     }
     res.status(200).json({
         success: true,
--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import product from './routes/productRoutes.js';
+import errorHandlingMiddleware from './middleware/error.js';
 const app = express();
 
 
@@ -9,6 +10,9 @@ app.use(express.json()); // To parse JSON bodies
 //Routes
 app.use('/api/v1', product);
 
+// Error handling middleware
+
+app.use(errorHandlingMiddleware);
 
 export default app;
 
