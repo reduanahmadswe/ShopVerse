8937685b65676dfd77cb698e887fd72d8ad33ca6
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-15 18:43:12 +0600

    refactor: reorganize error handling for uncaught exceptions in server setup

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,22 @@ import {connectMOngoDatabase} from './config/db.js';
 dotenv.config({ path: 'backend/config/config.env' });
 
 connectMOngoDatabase();
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.error(`Error: ${err.message}`);
+  console.error('Shutting down the server due to uncaught exception');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 const port = process.env.PORT || 3000;
-
-
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err) => {
