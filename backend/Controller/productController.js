3aa4f3cd8b0d9facdda43c4d48d8e01a058f1279
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-15 17:52:04 +0600

    enhance product routes by adding getSingleProduct endpoint and refactor code for clarity

--- backend/Controller/productController.js
+++ backend/Controller/productController.js
@@ -3,31 +3,18 @@ import Product from "../models/productModel.js";
 //Creating Product
 export const createProducts = async (req, res) => {
 
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json({
-      success: true,
-      product,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-
-  //jsom format for the created product
-//   {
-//     "name" : "Product1",
-//     "description" : "Product1 description",
-//     "price": 100,
-//     "images" : {
-//         "public_id" : "This is test id1",
-//         "url" : "This is test url 1"
-//     },
-//     "category" : "Shart"
-//     }
-
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json({
+            success: true,
+            product,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
 
 };
 
@@ -42,28 +29,27 @@ export const getAllProducts = async (req, res) => {
             message: "No products found",
         });
     }
-  res.status(200).json({
-    success: true,
-    products: products,
-    // count: products.length,
-    // message: "All products fetched successfully",
+    res.status(200).json({
+        success: true,
+        products: products,
+        // count: products.length,
+        // message: "All products fetched successfully",
 
-  });
+    });
 };
 
 //Update product
 export const updateProduct = async (req, res) => {
-    let products = await Product.findById(req.params.id);
+    const products = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
     if (!products) {
         return res.status(404).json({
             success: false,
             message: "Product not found",
         });
     }
-    products = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-    });
     res.status(200).json({
         success: true,
         products,
@@ -73,18 +59,35 @@ export const updateProduct = async (req, res) => {
 
 //Delete product
 export const deleteProduct = async (req, res) => {
-    let product = await Product.findById(req.params.id);
+
+    const product = await Product.findByIdAndDelete(req.params.id);
     if (!product) {
         return res.status(404).json({
             success: false,
             message: "Product not found",
         });
     }
-    product = await Product.findByIdAndDelete(req.params.id);
 
     res.status(200).json({
         success: true,
         message: "Product deleted successfully",
     });
 };
+
+
+//Accessing a single product
+export const getSingleProduct = async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found",
+        });
+    }
+    res.status(200).json({
+        success: true,
+        product,
+    });
+};
+
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import { createProducts, deleteProduct, getAllProducts, updateProduct} from '../Controller/productController.js';
+import { createProducts, deleteProduct, getAllProducts, getSingleProduct, updateProduct} from '../Controller/productController.js';
+
 const router = express.Router();
 
 
 
 //Routes
-router.route('/products').get(getAllProducts).post(createProducts);
-router.route('/product/:id').put(updateProduct).delete(deleteProduct);
+router.route('/products').get(getAllProducts)
+.post(createProducts);
+
+
+router.route('/product/:id')
+.put(updateProduct)
+.delete(deleteProduct)
+.get(getSingleProduct);
 
 
 
