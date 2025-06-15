442885cc595bff2c371786d54bda62169d11b808
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-15 18:49:41 +0600

    refactor: enhance error handling middleware with additional error cases

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,11 +1,39 @@
-
+import HandleError from '../utils/handleError.js';
 
 export default (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
-    
+
+    // CastError
+    if (err.name === 'CastError') {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new HandleError(message, 400);
+    }
+    // Mongoose Duplicate Key Error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new HandleError(message, 400);
+    }
+    // Mongoose Validation Error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message).join(', ');
+        err = new HandleError(message, 400);
+    }
+
+    // JsonWebTokenError
+    if (err.name === 'JsonWebTokenError') {
+        const message = 'Json Web Token is invalid, try again';
+        err = new HandleError(message, 400);
+    }
+    // TokenExpiredError
+    if (err.name === 'TokenExpiredError') {
+        const message = 'Json Web Token is expired, try again';
+        err = new HandleError(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
+        //  message: err.stack
     });
 }
