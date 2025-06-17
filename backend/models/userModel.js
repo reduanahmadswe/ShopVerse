afca9c608318cb9600fa2ed9abf6f98e1232dd4f
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-17 22:38:23 +0600

    feat: implement user model with schema definition for user attributes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,37 @@
 ///////////////////////////////////////
 ///////////    3:44:43      ///////////
 ///////////////////////////////////////
+//user model
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minlength: [6, "Password must be at least 6 characters long"],
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    
+});
+
+export default mongoose.model("User", userSchema);
