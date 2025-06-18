4834d55cb125cdee2149b8158b623d5ac12229b1
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-18 15:17:43 +0600

    add user routes and implement user registration functionality

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -4,25 +4,28 @@ import User from "../models/userModel.js";
 export const registerUser = handleAsyncError(async (req, res) => {
     try {
         const { name, email, password } = req.body;
+        const user = await User.create({
+            name,
+            email,
+            password,
+            avatar: {
+                public_id: "this is a sample id",
+                url: "this is a sample url",
+            }
+    
+        });
+
+        res.status(201).json({
+            success: true,
+            user,
+        });
+    
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-        avatar: {
-            public_id: "this is a sample id",
-            url: "this is a sample url",
-        }
+    
 
-    });
-
-    res.status(201).json({
-        success: true,
-        user,
-    });
-
+   
 });
 
--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import product from './routes/productRoutes.js';
 import errorHandlingMiddleware from './middleware/error.js';
+import user from './routes/userRoutes.js';
 const app = express();
 
 
@@ -9,6 +10,7 @@ app.use(express.json()); // To parse JSON bodies
 
 //Routes
 app.use('/api/v1', product);
+app.use('/api/v1', user);
 
 // Error handling middleware
 
