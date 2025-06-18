0586fa3bd1653d4c08aa322a3a8d3861152f901e
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-18 14:40:42 +0600

    feat: enhance user model validation with additional fields and improve error handling

---
+++ backend/Controller/userController.js
@@ -0,0 +1,28 @@
+import handleAsyncError from "../middleware/handleAsyncError.js";
+import User from "../models/userModel.js";
+
+export const registerUser = handleAsyncError(async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: "this is a sample id",
+            url: "this is a sample url",
+        }
+
+    });
+
+    res.status(201).json({
+        success: true,
+        user,
+    });
+
+});
+
