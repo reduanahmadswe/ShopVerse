6aecc23b84f6a53f93aee9197ea3356a60b1813e
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-22 04:26:16 +0600

    correct error message formatting and enhance user authentication verification logic

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -1,12 +1,19 @@
 import HandleError from '../utils/handleError.js';
 import handleAsyncError from './handleAsyncError.js';
-
+import jwt from "jsonwebtoken"
+import User from '../models/userModel.js';
 
 export const verifyUserAuth = handleAsyncError(async (req, res, next) => {
     const {token} = req.cookies;
     console.log(token);
 
     if(!token){
-        return next(new HandleError("Authentication is missing ! p\Plwase login to acces resource",401))
+        return next(new HandleError("Authentication is missing! Please login to acces resource",401))
     }
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log(decodedData);
+    req.user = await User.findById(decodedData.id);
+    next();
+
 });
