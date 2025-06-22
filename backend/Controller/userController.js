9e1be9b0414a9d781e991b848165aae3c89c4db6
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-23 02:27:11 +0600

    implement reset password functionality and update user routes to include reset endpoint

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -3,7 +3,7 @@ import User from "../models/userModel.js";
 import HandleError from "../utils/handleError.js";
 import { sendToken } from "../utils/jwtToken.js";
 import { sendEmail } from "../utils/sendEmail.js";
-
+import crypto from "crypto";
 
 // Register
 export const registerUser = handleAsyncError(async (req, res) => {
@@ -62,7 +62,7 @@ export const logout = handleAsyncError(async (req, res, next) => {
     });
 });
 
-//Reset Password
+//Forgot Password 
 export const requestPasswordReset = handleAsyncError(async (req, res, next) => {
     const { email } = req.body;
 
@@ -113,3 +113,46 @@ export const requestPasswordReset = handleAsyncError(async (req, res, next) => {
         return next(new HandleError("Could not save reset token. Please try again later.", 500));
     }
 });
+
+
+//Reset Password
+export const resetPassword = handleAsyncError(async (req, res, next) => {
+
+    // console.log(req.params.token);
+
+    const resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(req.params.token)
+        .digest('hex');
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return next(new HandleError("Reset token is invalid or has expired", 400));
+    }
+
+    const { password, confirmPassword } = req.body;
+    if (!password || !confirmPassword) {
+        return next(new HandleError("Please provide both password and confirm password", 400));
+    }
+
+    if (password !== confirmPassword) {
+        return next(new HandleError("Passwords do not match", 400));
+    }
+
+    user.password = password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res);
+    ///////////////////////////////////////
+///////////    06:38:43      ///////////
+///////////////////////////////////////
+
+
+})
