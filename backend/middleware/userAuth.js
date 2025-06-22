f0800592218c3fbcb5c21651adc2c0dc09a7d169
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-06-22 14:02:43 +0600

    add role-based access control to product routes and implement logout functionality in user routes

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -3,12 +3,13 @@ import handleAsyncError from './handleAsyncError.js';
 import jwt from "jsonwebtoken"
 import User from '../models/userModel.js';
 
+
 export const verifyUserAuth = handleAsyncError(async (req, res, next) => {
-    const {token} = req.cookies;
+    const { token } = req.cookies;
     console.log(token);
 
-    if(!token){
-        return next(new HandleError("Authentication is missing! Please login to acces resource",401))
+    if (!token) {
+        return next(new HandleError("Authentication is missing! Please login to acces resource", 401))
     }
 
     const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
@@ -17,3 +18,13 @@ export const verifyUserAuth = handleAsyncError(async (req, res, next) => {
     next();
 
 });
+
+export const roleBasedAccess = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new HandleError(`Role - ${req.user.role} is not allowed to access the resources`, 403));
+        }
+        next();
+    }
+}
+
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createProducts, deleteProduct, getAllProducts, getSingleProduct, updateProduct} from '../Controller/productController.js';
-import { verifyUserAuth } from '../middleware/userAuth.js';
+import { roleBasedAccess, verifyUserAuth } from '../middleware/userAuth.js';
 const router = express.Router();
 
 
@@ -8,14 +8,14 @@ const router = express.Router();
 //Routes
 router.route('/products')
 .get(verifyUserAuth,getAllProducts)
-.post(createProducts);
+.post(verifyUserAuth,roleBasedAccess('admin') ,createProducts);
 
 
 
 router.route('/product/:id')
-.put(updateProduct)
-.delete(deleteProduct)
-.get(getSingleProduct);
+.put(verifyUserAuth,updateProduct)
+.delete(verifyUserAuth,deleteProduct)
+.get(verifyUserAuth,getSingleProduct);
 
 
 
